62a20f2ee7493697d0050e28c8dd98e54fb03b4d
Austine <okonkwo.obele@gmail.com>
2022-11-10 17:14:37 +0100

    initial commit

---
+++ chapterThree/Account.java
@@ -0,0 +1,35 @@
+public class Account{
+	private String name;
+	private double balance;
+
+
+	public Account(){
+
+
+	}
+	public Account(String name, double balance){
+	this.name=name;
+	this.balance=balance;
+
+
+	}
+
+
+	public String getName(){
+	return this.name;
+
+	}
+	public double getBalance(){
+	return this.balance;
+	}
+
+	public String setName(){
+	this.name=name;
+}
+
+	public double setBalance(){
+	this.balance = balance
+}
+
+}
+
